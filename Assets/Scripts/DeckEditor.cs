6ae2a60e9b7c5daa6fda94c36688536a6b529366
hangman_on_benzene <98060908+JustBenzene@users.noreply.github.com>
2022-12-27 17:55:49 +0100

    choosing character done

--- Assets/Scripts/DeckEditor.cs
+++ Assets/Scripts/DeckEditor.cs
@@ -10,24 +10,34 @@ public class DeckEditor : MonoBehaviour
     [SerializeField] GameObject numberOnScreen;
     [SerializeField] GameObject nameField;
     [SerializeField] GameObject middle;
+
     [SerializeField] GameObject overlays;
     [SerializeField] GameObject saveScreen;
     [SerializeField] GameObject chooseCharacter;
 
     [SerializeField] GameObject chosen;
-    [SerializeField] GameObject chosen2;
+    [SerializeField] GameObject chosenSmall;
+    [SerializeField] GameObject chosenText;
     [SerializeField] Sprite[] characterSprites;
+    string[] characterNames = new string[] { "  Knight", " Assassin", " Sorcerer", "  Herald" };
+
+    [SerializeField] GameObject[] chosenCardPoolColor;
+    [SerializeField] GameObject[] chosenCardPoolText;
+    [SerializeField] GameObject[] chosenDeck;
 
     [SerializeField] GameObject[] lines;
 
-    int number;
+    int number = GameManager.deckNumber;
+
+    int[] deckCharacter = new int[] { 0, 1, 2, 3, 0 };
+    int chosenDecknumber = 4;
+    int chosenCharacter = 0;
 
     private void Start()
     {
-        float temp = 718f + (GameManager.aspectRatio - 1.334545f) * 797f * 1.004405f;
+        float temp = 568f + (GameManager.aspectRatio - 1.334545f) * 797f * 1.004405f;
         middle.GetComponent<RectTransform>().sizeDelta = new Vector2(800, temp);
 
-        number = GameManager.deckNumber;
         numberOnScreen.GetComponent<TextMeshProUGUI>().SetText(number.ToString());
 
         temp = 1f + (GameManager.aspectRatio - 1.334545f) * 0.2f * 1.004405f;
@@ -55,6 +65,7 @@ public class DeckEditor : MonoBehaviour
                 lines[line].SetActive(false);
             }
         }
+        lines[0].SetActive(true);
     }
 
     public void OnClickDontSafe()
@@ -83,11 +94,47 @@ public class DeckEditor : MonoBehaviour
 
     public void OnClickCharacter()
     {
+        chosen.GetComponent<Image>().sprite = characterSprites[chosenCharacter];
         chooseCharacter.SetActive(true);
     }
 
     public void OnClickChosen()
     {
+        if (chosenDecknumber == 4)
+        {
+            int temp = 0;
+            for (int character = 0; character < 4; character++)
+            {
+                if (deckCharacter[character] == deckCharacter[4])
+                {
+                    temp = character;
+                }
+            }
+            for (int character = 0; character < 4; character++)
+            {
+                if (deckCharacter[character] == chosenCharacter)
+                {
+                    deckCharacter[character] = deckCharacter[chosenDecknumber];
+                }
+            }
+            deckCharacter[temp] = chosenCharacter;
+            deckCharacter[4] = chosenCharacter;
+        }
+        else
+        {
+            for (int character = 0; character < 4; character++)
+            {
+                if (deckCharacter[character] == chosenCharacter)
+                {
+                    deckCharacter[character] = deckCharacter[chosenDecknumber];
+                }
+            }
+            if (deckCharacter[chosenDecknumber] == deckCharacter[4])
+            {
+                deckCharacter[4] = chosenCharacter;
+            }
+            deckCharacter[chosenDecknumber] = chosenCharacter;
+        }
         chooseCharacter.SetActive(false);
     }
 
@@ -111,9 +158,99 @@ public class DeckEditor : MonoBehaviour
         characterChosen(3);
     }
 
-    void characterChosen(int character)
+    void characterChosen(int pickcharacter)
     {
-        chosen.GetComponent<Image>().sprite = characterSprites[character];
-        chosen2.GetComponent<Image>().sprite = characterSprites[character];
+        chosen.GetComponent<Image>().sprite = characterSprites[pickcharacter];
+        chosenSmall.GetComponent<Image>().sprite = characterSprites[pickcharacter];
+        chosenText.GetComponent<TMP_Text>().text = characterNames[pickcharacter];
+        chosenCharacter = pickcharacter;
+    }
+
+    public void OnClickAllCards()
+    {
+        Deck(4);
+    }
+
+    public void OnClickDeck1()
+    {
+        Deck(0);
+    }
+
+    public void OnClickDeck2()
+    {
+        Deck(1);
+    }
+
+    public void OnClickDeck3()
+    {
+        Deck(2);
+    }
+
+    public void OnClickDeck4()
+    {
+        Deck(3);
+    }
+
+    void Deck(int deckNumber)
+    {
+        for (int deck = 0; deck < 5; deck++)
+        {
+            if (deck == deckNumber)
+            {
+                chosenCardPoolColor[deck].GetComponent<Image>().color = new Color32(0, 0, 0, 255);
+                chosenCardPoolText[deck].GetComponent<TMP_Text>().color = new Color32(255, 255, 255, 255);
+                chosenSmall.GetComponent<Image>().sprite = characterSprites[deckCharacter[deck]];
+                chosenText.GetComponent<TMP_Text>().text = characterNames[deckCharacter[deck]];
+                chosenCharacter = deckCharacter[deck];
+                chosenDecknumber = deck;
+            }
+            else
+            {
+                chosenCardPoolColor[deck].GetComponent<Image>().color = new Color32(255, 255, 255, 255);
+                chosenCardPoolText[deck].GetComponent<TMP_Text>().color = new Color32(50, 50, 50, 255);
+            }
+        }
+    }
+
+    public void OnClickSelect1()
+    {
+        Select(0);
+    }
+
+    public void OnClickSelect2()
+    {
+        Select(1);
+    }
+
+    public void OnClickSelect3()
+    {
+        Select(2);
+    }
+
+    public void OnClickSelect4()
+    {
+        Select(3);
+    }
+
+    void Select(int deckNumber)
+    {
+        for (int deck = 0; deck < 4; deck++)
+        {
+            if (deck == deckNumber)
+            {
+                chosenDeck[deck].GetComponent<Image>().color = new Color32(100, 100, 100, 255);
+                deckCharacter[4] = deckCharacter[deck];
+                if (chosenDecknumber == 4)
+                {
+                    chosenSmall.GetComponent<Image>().sprite = characterSprites[deckCharacter[deck]];
+                    chosenText.GetComponent<TMP_Text>().text = characterNames[deckCharacter[deck]];
+                    chosenCharacter = deckCharacter[deck];
+                }
+            }
+            else
+            {
+                chosenDeck[deck].GetComponent<Image>().color = new Color32(255, 255, 255, 255);
+            }
+        }
     }
 }
